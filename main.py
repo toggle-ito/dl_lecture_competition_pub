ba50cbabb4d23b0c00caf466ee263f41b91acf33
toggle-ito <i@toggle.co.jp>
2024-07-17 14:15:53 +0900

    submission commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn  
 import hydra
 from omegaconf import DictConfig
 from torch.utils.data import DataLoader
@@ -43,6 +44,32 @@ def save_optical_flow_to_npy(flow: torch.Tensor, file_name: str):
     file_name: str => ファイル名
     '''
     np.save(f"{file_name}.npy", flow.cpu().numpy())
+
+def train_one_epoch(model, dataloader, optimizer, criterion, device):
+    model.train()
+    total_loss = 0.0
+
+    for inputs in dataloader:
+        event_image = inputs["event_volume"].to(device).float()  # データ型を Float に変換
+        ground_truth_flow = inputs["flow_gt"].to(device).float()  # データ型を Float に変換
+
+        optimizer.zero_grad()
+        flow_dict, intermediate_outputs = model(event_image)
+
+        # 複数スケールでの損失計算
+        loss = 0.0
+        for i, output in enumerate(intermediate_outputs):
+            # 出力サイズにターゲットをリサイズ
+            scaled_targets = nn.functional.interpolate(ground_truth_flow, size=output.shape[2:], mode='bilinear', align_corners=False)
+            loss += criterion(output, scaled_targets)
+
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+
+    return total_loss / len(dataloader)
+
 
 @hydra.main(version_base=None, config_path="configs", config_name="base")
 def main(args: DictConfig):
@@ -107,37 +134,21 @@ def main(args: DictConfig):
         Key: seq_name, Type: list
         Key: event_volume, Type: torch.Tensor, Shape: torch.Size([Batch, 4, 480, 640]) => イベントデータのバッチ
     '''
-    # ------------------
-    #       Model
-    # ------------------
+
     model = EVFlowNet(args.train).to(device)
 
-    # ------------------
-    #   optimizer
-    # ------------------
     optimizer = torch.optim.Adam(model.parameters(), lr=args.train.initial_learning_rate, weight_decay=args.train.weight_decay)
-    # ------------------
-    #   Start training
-    # ------------------
+
     model.train()
+
+
+    criterion = nn.MSELoss()
+
     for epoch in range(args.train.epochs):
-        total_loss = 0
         print("on epoch: {}".format(epoch+1))
-        for i, batch in enumerate(tqdm(train_data)):
-            batch: Dict[str, Any]
-            event_image = batch["event_volume"].to(device) # [B, 4, 480, 640]
-            ground_truth_flow = batch["flow_gt"].to(device) # [B, 2, 480, 640]
-            flow = model(event_image) # [B, 2, 480, 640]
-            loss: torch.Tensor = compute_epe_error(flow, ground_truth_flow)
-            print(f"batch {i} loss: {loss.item()}")
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        train_loss = train_one_epoch(model, train_data, optimizer, criterion, device)
+        print(f'Epoch {epoch+1}, Loss: {train_loss:.4f}')
 
-            total_loss += loss.item()
-        print(f'Epoch {epoch+1}, Loss: {total_loss / len(train_data)}')
-
-    # Create the directory if it doesn't exist
     if not os.path.exists('checkpoints'):
         os.makedirs('checkpoints')
     
@@ -146,9 +157,6 @@ def main(args: DictConfig):
     torch.save(model.state_dict(), model_path)
     print(f"Model saved to {model_path}")
 
-    # ------------------
-    #   Start predicting
-    # ------------------
     model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval()
     flow: torch.Tensor = torch.tensor([]).to(device)
@@ -157,9 +165,11 @@ def main(args: DictConfig):
         for batch in tqdm(test_data):
             batch: Dict[str, Any]
             event_image = batch["event_volume"].to(device)
-            batch_flow = model(event_image) # [1, 2, 480, 640]
+            _, batch_flow = model(event_image)
+            batch_flow = batch_flow[-1]  
             flow = torch.cat((flow, batch_flow), dim=0)  # [N, 2, 480, 640]
         print("test done")
+
     # ------------------
     #  save submission
     # ------------------
--- src/models/evflownet.py
+++ src/models/evflownet.py
@@ -7,27 +7,24 @@ _BASE_CHANNELS = 64
 
 class EVFlowNet(nn.Module):
     def __init__(self, args):
-        super(EVFlowNet,self).__init__()
+        super(EVFlowNet, self).__init__()
         self._args = args
 
-        self.encoder1 = general_conv2d(in_channels = 4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder2 = general_conv2d(in_channels = _BASE_CHANNELS, out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder3 = general_conv2d(in_channels = 2*_BASE_CHANNELS, out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-        self.encoder4 = general_conv2d(in_channels = 4*_BASE_CHANNELS, out_channels=8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder1 = general_conv2d(in_channels=4, out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder2 = general_conv2d(in_channels=_BASE_CHANNELS, out_channels=2 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder3 = general_conv2d(in_channels=2 * _BASE_CHANNELS, out_channels=4 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.encoder4 = general_conv2d(in_channels=4 * _BASE_CHANNELS, out_channels=8 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
 
-        self.resnet_block = nn.Sequential(*[build_resnet_block(8*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm) for i in range(2)])
+        self.resnet_block = nn.Sequential(*[build_resnet_block(8 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm) for i in range(2)])
 
-        self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16*_BASE_CHANNELS,
-                        out_channels=4*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
-        self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8*_BASE_CHANNELS+2,
-                        out_channels=2*_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
-        self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4*_BASE_CHANNELS+2,
-                        out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
-
-        self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2*_BASE_CHANNELS+2,
-                        out_channels=int(_BASE_CHANNELS/2), do_batch_norm=not self._args.no_batch_norm)
+        self.decoder1 = upsample_conv2d_and_predict_flow(in_channels=16 * _BASE_CHANNELS,
+                                                         out_channels=4 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder2 = upsample_conv2d_and_predict_flow(in_channels=8 * _BASE_CHANNELS + 2,
+                                                         out_channels=2 * _BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder3 = upsample_conv2d_and_predict_flow(in_channels=4 * _BASE_CHANNELS + 2,
+                                                         out_channels=_BASE_CHANNELS, do_batch_norm=not self._args.no_batch_norm)
+        self.decoder4 = upsample_conv2d_and_predict_flow(in_channels=2 * _BASE_CHANNELS + 2,
+                                                         out_channels=int(_BASE_CHANNELS / 2), do_batch_norm=not self._args.no_batch_norm)
 
     def forward(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
         # encoder
@@ -46,51 +43,26 @@ class EVFlowNet(nn.Module):
 
         # decoder
         flow_dict = {}
+        intermediate_outputs = []
+
         inputs = torch.cat([inputs, skip_connections['skip3']], dim=1)
         inputs, flow = self.decoder1(inputs)
         flow_dict['flow0'] = flow.clone()
+        intermediate_outputs.append(flow)
 
         inputs = torch.cat([inputs, skip_connections['skip2']], dim=1)
         inputs, flow = self.decoder2(inputs)
         flow_dict['flow1'] = flow.clone()
+        intermediate_outputs.append(flow)
 
         inputs = torch.cat([inputs, skip_connections['skip1']], dim=1)
         inputs, flow = self.decoder3(inputs)
         flow_dict['flow2'] = flow.clone()
+        intermediate_outputs.append(flow)
 
         inputs = torch.cat([inputs, skip_connections['skip0']], dim=1)
         inputs, flow = self.decoder4(inputs)
         flow_dict['flow3'] = flow.clone()
+        intermediate_outputs.append(flow)
 
-        return flow
-        
-
-# if __name__ == "__main__":
-#     from config import configs
-#     import time
-#     from data_loader import EventData
-#     '''
-#     args = configs()
-#     model = EVFlowNet(args).cuda()
-#     input_ = torch.rand(8,4,256,256).cuda()
-#     a = time.time()
-#     output = model(input_)
-#     b = time.time()
-#     print(b-a)
-#     print(output['flow0'].shape, output['flow1'].shape, output['flow2'].shape, output['flow3'].shape)
-#     #print(model.state_dict().keys())
-#     #print(model)
-#     '''
-#     import numpy as np
-#     args = configs()
-#     model = EVFlowNet(args).cuda()
-#     EventDataset = EventData(args.data_path, 'train')
-#     EventDataLoader = torch.utils.data.DataLoader(dataset=EventDataset, batch_size=args.batch_size, shuffle=True)
-#     #model = nn.DataParallel(model)
-#     #model.load_state_dict(torch.load(args.load_path+'/model18'))
-#     for input_, _, _, _ in EventDataLoader:
-#         input_ = input_.cuda()
-#         a = time.time()
-#         (model(input_))
-#         b = time.time()
-#         print(b-a)
+        return flow_dict, intermediate_outputs
